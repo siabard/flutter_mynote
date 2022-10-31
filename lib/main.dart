6ac33d58245a54b40b317cdc38d43cdc841bd797
Yeonho Jang <siabard@gmail.com>
2022-11-01 08:51:11 +0900

    Firebase 초기화 루틴 위치 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'view/login_view.dart';
 import 'view/register_view.dart';
-
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RegisterView(),
-    );
+    return Scaffold(
+        appBar: AppBar(title: const Text("Home")),
+        body: FutureBuilder(
+            future: Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  final user = FirebaseAuth.instance.currentUser;
+                  final emailVerified = user?.emailVerified ?? false;
+                  if (emailVerified == true) {
+                    print('You are a verified user');
+                  } else {
+                    print('Your email is not verified');
+                  }
+
+                  return const Text("Done");
+                default:
+                  return const Text("Loading...");
+              }
+            }));
   }
 }
