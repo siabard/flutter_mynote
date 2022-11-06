c7081d5b1ca2a214380159b4cfa3d8360f068423
Yeonho Jang <siabard@gmail.com>
2022-11-07 08:39:10 +0900

    로그인 후 이동하는 부분 수정

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -30,49 +31,56 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: Column(children: [
-      TextField(
-          controller: _email,
-          enableSuggestions: false,
-          autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(hintText: 'Enter your email here')),
-      TextField(
-          controller: _password,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration:
-              const InputDecoration(hintText: 'Enter your password here')),
-      TextButton(
-          onPressed: () async {
-            final email = _email.text;
-            final password = _password.text;
-            try {
-              final userCredential = await FirebaseAuth.instance
-                  .signInWithEmailAndPassword(email: email, password: password);
-              print(userCredential);
-            } on FirebaseAuthException catch (e) {
-              if (e.code == 'user-not-found') {
-                print("User not found");
-              } else if (e.code == 'wrong-password') {
-                print('Wrong password');
-              }
-            } catch (e) {
-              print("something bad happend!");
-              print(e.runtimeType);
-            }
-          },
-          child: const Text("Login")),
-      TextButton(
-          onPressed: () {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              '/register/',
-              (route) => false,
-            );
-          },
-          child: const Text("Not registered yet? Register here!"))
-    ]));
+        appBar: AppBar(title: const Text("Login")),
+        body: Column(children: [
+          TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your email here')),
+          TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your password here')),
+          TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                try {
+                  final userCredential =
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email,
+                    password: password,
+                  );
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/notes/',
+                    (route) => false,
+                  );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'user-not-found') {
+                    devtools.log("User not found");
+                  } else if (e.code == 'wrong-password') {
+                    devtools.log('Wrong password');
+                  }
+                } catch (e) {
+                  devtools.log("something bad happend!");
+                  devtools.log(e.runtimeType.toString());
+                }
+              },
+              child: const Text("Login")),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/register/',
+                  (route) => false,
+                );
+              },
+              child: const Text("Not registered yet? Register here!"))
+        ]));
   }
 }
