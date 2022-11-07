32ea1130d057a356926aa9ed287603c86fa1b3e0
Yeonho Jang <siabard@gmail.com>
2022-11-08 08:20:21 +0900

    에러 메세지 추출

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import '../constants/routes.dart';
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -65,13 +64,26 @@ class _LoginViewState extends State<LoginView> {
                   );
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
-                    devtools.log("User not found");
+                    await showErrorDialog(
+                      context,
+                      'User not found',
+                    );
                   } else if (e.code == 'wrong-password') {
-                    devtools.log('Wrong password');
+                    await showErrorDialog(
+                      context,
+                      'Wrong credentials',
+                    );
+                  } else {
+                    await showErrorDialog(
+                      context,
+                      'Error: ${e.code}',
+                    );
                   }
                 } catch (e) {
-                  devtools.log("something bad happend!");
-                  devtools.log(e.runtimeType.toString());
+                  await showErrorDialog(
+                    context,
+                    e.toString(),
+                  );
                 }
               },
               child: const Text("Login")),
