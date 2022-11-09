e2e7ec5306fd8677e8028655bbf422ecc5b593a6
Yeonho Jang <siabard@gmail.com>
2022-11-09 09:14:58 +0900

    Auth Service 구성

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+
+// Generic exception
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
